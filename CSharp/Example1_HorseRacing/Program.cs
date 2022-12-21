8de26d46a4cb9e9a8e9a41984504e517234c8501
rudwo97 <120106112+rudwo97@users.noreply.github.com>
2022-12-21 21:54:54 +0900

    C# - Structure

--- CSharp/Example1_HorseRacing/Program.cs
+++ CSharp/Example1_HorseRacing/Program.cs
@@ -41,11 +41,17 @@ namespace Example1_HorseRacing
             // 이름 다 설정 해줌 (이름 패턴은 경주마1, 경주마2, 경주마3 ... )
 
             Horse[] horses = new Horse[5];
+            Horse[] horsesFinished = new Horse[5];
+            int grade = 0;
 
             for (int i = 0; i < horses.Length; i++)
             {
                 horses[i] = new Horse();
+                horses[i].Name = $"경주마{i + 1}"; 
             }
+
+            Console.WriteLine("============================ 경주 시작 ========================");
+
 
             while (gameFinished == false)
             {
@@ -53,13 +59,46 @@ namespace Example1_HorseRacing
                 // 각각의 말은 거리 200에 도달하면 도착해서 더이상 전진하지 않고 
                 // 매초 각 말들이 아직 달리고 있다면 달린 거리를, 도착했다면 도착 상태를 콘솔창에 출력 해줍니다.
                 // 모든 말이 도착했으면 게임 끝냄
-                random = new Random();
-                int tmpSpeed = random.Next(minSpeed, maxSpeed + 1);
-                Console.WriteLine("test");
+                for (int i = 0; i < horses.Length; i++)
+                {
+                    // 경주가 끝난 말
+                    if (horses[i].IsFinished)
+                    {
+                        Console.WriteLine($"{horses[i].Name} 은 도착했다.");
+                    }
+                    // 경주가 아직 끝나지 않은말은 달리게 한다
+                    else
+                    {
+                        random = new Random();
+                        horses[i].Run(random.Next(minSpeed, maxSpeed + 1)); // 랜덤속도로 전진
+
+                        Console.WriteLine($"{horses[i].Name} : 현재 거리 {horses[i].TotalDistance}");
+
+                        // 전진했는데 도착했으면 끝났다고 해줌   
+                        if (horses[i].TotalDistance >= goalPos)
+                        {
+                            horses[i].IsFinished = true;
+                            horsesFinished[grade++] = horses[i];
+                        }
+                    }
+                }
+
+                // 모든말이 도착했는지 체크
+                if (grade >= horses.Length)
+                    break;
+
+                
                 Thread.Sleep(1000);
+                sec++;
+                Console.WriteLine($"=============================== {sec} 경과 ===================================);
             }
 
+            Console.WriteLine("======================= 경주 끝 ===============================");
             // 각 말들을 등수 순서대로 이름을 출력해줌
+            for (int i = 0; i < horsesFinished.Length; i++)
+            {
+                Console.WriteLine($"{i + 1} 등 : {horsesFinished[i].Name}");
+            }
         }
     }
 }
---
+++ CSharp/Structure/Program.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+// 구조체 사용자 정의 자료형
+// 멤버들을 캡슐화하는 자료형
+// 멤버 변수, 프로퍼티, 함수 ..
+//
+// 선언 형태
+// struct 이름
+// {
+//   멤버들 선언/정의
+// }
+
+namespace Structure
+{
+    public struct Stats
+    {
+        // 프로퍼티
+        // getter / setter (get 접근자 / set 접근자/ 를 구현할 수 있는 속성
+        public int STR
+        {
+            get
+            {
+                return _str;
+            }
+
+            set
+            {
+                if (value < 0)
+                    return;
+                
+
+                _str = value;
+            }
+        }
+
+        public static Stats Zero
+        {
+            get 
+            {
+                return new Stats(0, 0, 0, 0);
+            }
+        }
+
+        public static Stats Default => new Stats(10, 10, 10, 10);
+
+        private int _str;
+        private int _dex;
+        private int _int;
+        private int _luk;
+
+        // 오버로드 (OverLoad)
+        // 매개변수가 다르면서 동일한 이름의 함수를 정의할 수 있는 기능
+
+        // 생성자 오버로딩
+        // 구조체의 생성자에서는 모든 멤버를 초기화 해야함.
+        public Stats(int STR, int DEX, int INT, int LUK)
+        {
+            this._str = STR;
+            this._dex = DEX;
+            this._int = INT;
+            this._luk = LUK;  
+        }
+
+        public static Stats GetDefault()
+        {
+            return new Stats(10, 10, 10, 10);
+        }
+
+        public static Stats GetZero()
+        {
+            return new Stats(0, 0, 0, 0);
+        }
+
+        public void SetSTR(int value)
+        {
+            if (value < 0)
+                return;
+            this._str = value;
+        }
+
+        public int GetSTR()
+        {
+            return this._str;
+        }
+        public void SetDEX(int value)
+        {
+            if (value < 0)
+                return;
+            this._dex = value;
+        }
+
+        public int GetDEX()
+        {
+            return this._dex;
+        }
+        public void SetINT(int value)
+        {
+            if (value < 0)
+                return;
+            this._int = value;
+        }
+
+        public int GetINT()
+        {
+            return this._int;
+        }
+        public void SetLUK(int value)
+        {
+            if (value < 0)
+                return;
+            this._luk = value;
+        }
+
+        public int GetLUK()
+        {
+            return this._luk;
+        }
+        public int GetCombatPower()
+        {
+            return _str + _dex + _int + _luk;
+        }
+
+        public int GetCombatPower(int gain)
+        {
+            return GetCombatPower() * gain;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 클래스는 참조타입
+            Player player = new Player();
+            player.STR = 10;
+            player.DEX = 10;
+            player.INT = 10;
+            player.LUK = 10;
+
+            // 구조체는 값타입
+            Player_s player_s = new Player_s();
+            player_s.STR = 10;
+            player_s.DEX = 10;
+            player_s.INT = 10;
+            player_s.LUK = 10;
+
+            // 그러면 언제 클래스를 쓰고 언제 구조체를 쓰나?
+            // 1. 멤버들의 크기 총합이 16 byte 이상일때는 일반적으로 클래스를 씀.
+            // 
+            // 참조타입으로 값을 읽고 쓰는거 보다 값타입을 읽고 쓰는게 빠르다.
+            // 하지만 16 byte를 초과할때는 레지스터가 두번 이상 값을 읽어야 하기 때문에
+            // 값타입이어도 참조타입보다 느려진다.
+            //
+            // 2. 값을 쓰고 읽는것이 빈번하게 일어날 경우 (ex. 함수의 인자로 넘겨주는 횟수가 잦을 경우)
+            // 구조체를 사용하는것이 효율적이다.
+            Enemy enemy = new Enemy();
+            enemy.Stats.SetSTR = 10;
+            enemy.Stats.SetDEX = 10;
+            enemy.Stats.SetINT = 10;
+            enemy.Stats.SetLUK = 10;
+            enemy.Stats = new Stats(10, 10, 10, 10);
+            enemy.Stats = Stats.GetDefault();
+            Console.WriteLine($"적의 전투력이 {enemy.Stats.GetCombatPower()} 입니다 !!");
+
+           
+        }
+    }
+
+    public class Player
+    {
+        public int STR;
+        public int DEX;
+        public int INT;
+        public int LUK;
+    }
+    public struct Player_s
+    {
+        public int STR;
+        public int DEX;
+        public int INT;
+        public int LUK;
+    }
+
+    public class Enemy
+    {
+        public Stats Stats;
+    }
+}
