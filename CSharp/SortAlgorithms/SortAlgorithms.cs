b0d2d78fba844a49ffa7cc28e9fb14c0304e65f4
rudwo97 <120106112+rudwo97@users.noreply.github.com>
2023-01-13 21:56:02 +0900

    C# - SortAlgorithms 2

--- CSharp/SortAlgorithms/SortAlgorithms.cs
+++ CSharp/SortAlgorithms/SortAlgorithms.cs
@@ -1,21 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Reflection.Metadata;
-using System.Text.Json;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SortAlgorithms
 {
-    internal class SortAlgorithms
+    internal static class SortAlgorithms
     {
-        /// <summary>
-        ///  버블소트
-        ///  거품 정렬
-        ///  바로뒤의 요소가 현재요소보다 작으면 스왑
-        ///  Outer For loop 한번 돌 때마다 가장 큰 수의 위치가 하나씩 고정
-        ///  O(N^2)
-        ///  Stable : 값이 동일한 요소들을 정렬한 후에 기존 순서가 보장됨
-        /// </summary>
+       
 
+
+        /// <summary>
+        /// 거품 정렬
+        /// 바로뒤의 요소가 현재요소보다 작으면 스왑 
+        /// Outer For loop 한번 돌 때마다 가장 큰 수의 위치가 하나씩 고정 
+        /// O(N^2)
+        /// Stable : 값이 동일한 요소들을 정렬한 후에 기존 순서가 보장됨
+        /// </summary>
         public static void BubbleSort(int[] arr)
         {
             int i, j;
@@ -26,20 +29,20 @@ namespace SortAlgorithms
                     if (arr[j] > arr[j + 1])
                     {
                         Swap(ref arr[j], ref arr[j + 1]);
-                    }    
+                    }
                 }
             }
         }
 
         /// <summary>
         /// 선택 정렬
-        /// 현재의 바로뒤부터 끝가지 중에서 가장 작은 요소를 찾아서 스왑
-        /// Outer For loopo 한번 돌 때마다 가장 작은 수의 위치가 하나씩 고정
+        /// 현재의 바로뒤부터 끝까지 중에서 가장 작은 요소를 찾아서 스왑
+        /// Outer For loop 한번 돌 때마다 가장 작은 수의 위치가 하나씩 고정 
         /// O(N^2)
         /// Unstable
         /// </summary>
         /// <param name="arr"></param>
-        public static void SelectionsSort(int[] arr)
+        public static void SelectionSort(int[] arr)
         {
             int i, j, min;
             for (i = 0; i < arr.Length; i++)
@@ -58,8 +61,8 @@ namespace SortAlgorithms
 
         /// <summary>
         /// 삽입 정렬
-        /// 현재위치보다 이전 위치들중에서 더 큰값이 있으면 더 큰값으로 현재 위치에 덮어쓰고
-        /// 덮어쓰기가 끝나면 마지막으로 덮어쓸때 참조했던 위치에 기존 현재위치의 값을 덮어씀 (스왑)
+        /// 현재위치보다 이전 위치들중에서 더 큰값이 있으면 더 큰값으로 현재 위치에 덮어쓰고 
+        /// 덮어쓰기 끝나면 마지막으로 덮어쓸때 참조했던 위치에 기존 현재위치의 값을 덮어씀 (스왑)
         /// O(N^2)
         /// Stable
         /// </summary>
@@ -80,7 +83,101 @@ namespace SortAlgorithms
             }
         }
 
-        private static void Swap(ref int a, int b)
+        
+
+        #region Merge Sort
+        /// <summary>
+        /// 병합 정렬
+        /// 요소를 최소단위까지 나눈 후에 차례대로 병합을 하면서 정렬함. (Divide and conquer)
+        /// O(NLogN)
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void MergeSort(int[] arr)
+        {
+            MergeSort (arr, 0, arr.Length - 1);
+        }
+
+        public static void MergeSort(int[] arr, int start, int end)
+        {
+            if (start < end)
+            {
+                int mid = (start + end) / 2;
+
+                MergeSort(arr, start, mid);
+                MergeSort(arr, mid + 1, end);
+
+                Merge(arr, start, mid, end);
+            }
+        }
+
+        private static void Merge(int[] arr, int start, int mid, int end)
+        {
+            int[] tmp = new int[end + 1];
+            for (int i = 0; i < end + 1; i++)
+                tmp[i] = arr[i];
+
+            int part1 = start;
+            int part2 = mid + 1;
+            int index = start;
+
+            // part1 과 part2 비교해서 정렬하면서 채워넣음.
+            while (part1 <= mid && part2 <= end)
+            {
+                if (tmp[part1] <= tmp[part2])
+                {
+                    arr[index++] = tmp[part1++];
+                }
+                else
+                {
+                    arr[index++] = tmp[part2++];
+                }
+            }
+
+            // 남은 part1 들을 index 위치 뒤에 쭉 이어 붙여준다.
+            // 정복이 끝나고 남은 part2는 원본 배열 그대로 남아있으면 타당한 위치기때문에 따로 신경쓸 필요 없다.
+            for (int i = 0; i < mid - part1; i++)
+            {
+                arr[index + i] = tmp[part1 + i];
+            }
+        }
+        #endregion
+
+        #region Quick Sort
+
+        public static void QuickSort(int[] arr, int start, int end)
+        {
+            if (start < end)
+            {
+                int p = Partition(arr, start, end);
+            }
+        }
+
+        private static int Partition(int[] arr, int start, int end)
+        {
+            int pivot = arr[end + (start - end) / 2 - 1];
+
+            while (true)
+            {
+                while (arr[start] < pivot) start++;
+                while (arr[end] > pivot) end--;
+
+                if (start < end)
+                {
+                    Swap(ref arr[start], ref arr[end]);
+                }
+                else
+                {
+                    return end; // returnm pivot index
+                }
+            }
+
+        }
+
+
+
+        #endregion
+
+        private static void Swap(ref int a, ref int b)
         {
             int tmp = b;
             b = a;
